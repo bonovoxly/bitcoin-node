05af9d6c6cf7a081ed27a9adced5c07d5eef0a70
Bill Cawthra <bill.cawthra@pokitdok.com>
2017-11-30 21:19:49 -0500

    added -o so it only runs when repository changes

--- terraform/aws/user_data.sh
+++ terraform/aws/user_data.sh
@@ -2,5 +2,5 @@
 apt-get update
 apt-get -y install python-pip
 pip install ansible
-HOME=/root ansible-pull -d /opt/bitcoin-node -U https://github.com/bonovoxly/bitcoin-node /opt/bitcoin-node/ansible/bitcoin_node.yml >> /var/log/ansible-pull.log
+HOME=/root ansible-pull -o -d /opt/bitcoin-node -U https://github.com/bonovoxly/bitcoin-node /opt/bitcoin-node/ansible/bitcoin_node.yml >> /var/log/ansible-pull.log
 
--- terraform/gcloud/compute.tf
+++ terraform/gcloud/compute.tf
@@ -10,7 +10,7 @@ resource "google_compute_instance" "bitcoin-node" {
       type = "pd-standard"
     }
   }
-  metadata_startup_script = "apt-get update && apt-get -y install python-pip && pip install ansible && HOME=/root ansible-pull -d /opt/bitcoin-node -U https://github.com/bonovoxly/bitcoin-node /opt/bitcoin-node/ansible/bitcoin_node.yml >> /var/log/ansible-pull.log"
+  metadata_startup_script = "apt-get update && apt-get -y install python-pip && pip install ansible && HOME=/root ansible-pull -o -d /opt/bitcoin-node -U https://github.com/bonovoxly/bitcoin-node /opt/bitcoin-node/ansible/bitcoin_node.yml >> /var/log/ansible-pull.log"
   network_interface {
     network = "default"
     access_config {
